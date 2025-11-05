a232f19b7a9aeb0918ac2064a0c0d9451f2be5f0
SayMartin <martin@appfinningar.se>
2025-11-05 14:06:49 +0100

    minor update

--- src/pages/no-page.jsx
+++ src/pages/no-page.jsx
@@ -1,7 +1,7 @@
 export default function NoPage() {
   return (
     <div>
-      <h2 style={{ color: "red" }}>Error - Page is not found</h2>
+      <h2 style={{ color: "red" }}>Error - Page is not found. /WebCakery</h2>
     </div>
   );
 }
